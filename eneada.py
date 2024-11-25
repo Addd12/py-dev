0163806e53720d3fa91ee86ceb53e9192fd30e85
Addd12 <addd.github@gmail.com>
2024-11-25 11:49:24 +0100

    extracted titles containing specific string using list comprehension

--- eneada.py
+++ eneada.py
@@ -10,5 +10,36 @@ def fetch_api_data(api_url):
         print(f"The data could not be fetched as a result of {exception}")
         return None
 
+#2a extract titles from the api data, that contain a specific string
+def extract_titles(api_data):
+    print("----------------Extracting data using the first method-----------------------")
+    specific_str = input("Please type the string you want to find in the title (ex. qui, aut, porro): ")
+    titles = []
+    print(f"List of titles that contain {specific_str}: ")
+    for item in api_data:
+        #get only the items that contain a specific string
+        if specific_str in item['title']:
+            print(item['title'])
+            #then store them in a list
+            titles.append(item['title'])
+    return titles
+
+#2b extract titles from the api data, that contain a specific string using list comprehension
+def extract_titles_using_list_comp(api_data):
+    print("\n----------------Extracting data using list comprehension method-----------------------")
+    specific_str = input("Please type the string you want to find in the title (ex. qui, aut, porro): ")
+    print(f"List of titles that contain {specific_str}, extracted using list comprehension: ")
+
+    titles = [item['title'] for item in api_data if specific_str in item['title']]
+    for title in titles:
+        print(title)
+
+    return titles
+
+
 api_url = 'https://jsonplaceholder.typicode.com/todos'
-fetch_api_data(api_url)
+fetched_data = fetch_api_data(api_url)
+#print(type(fetched_data))
+extract_titles(fetched_data)
+
+extract_titles_using_list_comp(fetched_data)
